e4d93dc05c3e4dcc4f2a4c30903aafd7adaf82db
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-07-30 14:59:25 -0700

    build: configure ESLint to ignore extend entrypoint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
-const { overrides, ...config } = require('@hover/javascript/eslint')
+const {overrides, ...config} = require('@hover/javascript/eslint')
 
 module.exports = {
   ...config,
   ignorePatterns: ['dom-testing-library.js', 'rollup.input.js'],
   parserOptions: {
-    project: ['tsconfig.json', 'tsconfig.eslint.json'],
+    project: ['tsconfig.eslint.json'],
   },
 }
