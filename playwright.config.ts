34303fee02a377d11673fbafc59dc8a477a5fe3b
Jamie Rolfs <jamie.rolfs@gmail.com>
2022-09-23 00:18:33 -0700

    test: configure global `actionTimeout` for Playwright Test

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import {PlaywrightTestConfig} from '@playwright/test'
 const config: PlaywrightTestConfig = {
   reporter: 'list',
   testDir: 'test/fixture',
+  use: {actionTimeout: 3000},
 }
 
 export default config
